c9ec74f95cfaf5ed767c6ad40024cf8f4afe8a0e
Zachary Churchill <zacharyachurchill@gmail.com>
2022-04-06 20:46:13 -0400

    font rendering code

--- examples/text-editor.hs
+++ examples/text-editor.hs
@@ -1,12 +1,19 @@
+{-# LANGUAGE OverloadedStrings #-}
+
 module Main where
 
 import MyLib
 import Graphics.Gloss
 import Control.Monad.IO.Class
+import Graphics.Text.TrueType (FontStyle(FontStyle), FontDescriptor (FontDescriptor))
+import qualified Graphics.Text.TrueType as TT
+
+firaCode = FontDescriptor "Fira Sans" (FontStyle False False)
 
 main :: IO ()
-main = defaultMainWith defaultSettings $ do
-  didSave <- button (Text "Save") 5.0 2.0
+main = defaultMain $ do
+  didSaveText <- renderFont firaCode (TT.PointSize 16) "Save"
+  didSave <- button [didSaveText] 75.0 30.0
   if didSave
     then liftIO $ putStrLn "you saved!"
     else pure ()
--- src/MyLib.hs
+++ src/MyLib.hs
@@ -16,32 +16,66 @@ import Polysemy.Input
 import Polysemy.Output
 import Polysemy.Writer
 import Polysemy.Embed (runEmbedded)
+import Data.Vector (Vector)
+import qualified Data.Vector as V
+import qualified Data.Vector.Unboxed as VU
+import qualified Graphics.Text.TrueType as TT
+import Polysemy.State
+import Control.Monad.IO.Class (MonadIO(liftIO))
+
+data AppState = AppState
+  { fontCache :: TT.FontCache
+  }
+
+type GUIM = Sem '[GUI, State AppState, Embed IO]
 
 data GUI m a where
-  Button :: Picture -> Float -> Float -> GUI m Bool
+  Button :: [Picture] -> Float -> Float -> GUI m Bool
 
 makeSem ''GUI
 
 data Settings = Settings 
   { tickRate :: Int
   , bgColor :: Color
+  , mainWindow :: Display
   }
 
 defaultSettings :: Settings
 defaultSettings = Settings
   { tickRate = 60
   , bgColor = makeColorI 0xff 0xff 0xff 0xff
+  , mainWindow = InWindow "Wave" (800, 600) (100, 100)
   }
 
-defaultMainWith :: Settings -> Sem '[GUI, Embed IO] () -> IO ()
-defaultMainWith settings gui' = do
+defaultMain :: Sem '[GUI, State AppState, Embed IO] () -> IO ()
+defaultMain = mainWith defaultSettings
+
+newState :: IO AppState
+newState = AppState <$> TT.buildCache
+
+renderFont :: TT.FontDescriptor -> TT.PointSize -> String -> GUIM Picture
+renderFont fontd pt str = do 
+  state <- get
+  case TT.findFontInCache (fontCache state) fontd of
+    Nothing -> error $ unwords ["font", show fontd, "missing"]
+    Just fp -> do 
+      f <- either (error . show) id <$> (liftIO $ TT.loadFontFile fp)
+      pure $ foldMap (line . VU.toList) $ mconcat $ TT.getStringCurveAtPoint
+        96 -- DPI
+        (0.0, 0.0)
+        [(f,pt,str)]
+
+mainWith :: Settings -> GUIM () -> IO ()
+mainWith settings gui' = do
+  state <- newState
+  let
   playIO
-    FullScreen
+    (mainWindow settings)
     (bgColor settings)
     (tickRate settings)
     gui'
     (\gui -> do
-      gui2 <- runM $ render gui
+      gui2 <- runM $ evalState state $ render gui
       pure $ Pictures gui2
     )
     (\e gui -> pure $ gui)
@@ -61,4 +95,4 @@ runGUIPure sem = reinterpret go sem
   where
   go :: GUI (Sem rInitial) x -> Sem (Writer [Picture] : r) x
   go = \case
-    (Button p x y) -> tell [rectangleSolid x y] *> pure False
+    (Button p x y) -> tell (rectangleSolid x y : p) *> pure False
