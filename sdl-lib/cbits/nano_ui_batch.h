a1b7980325f7d33b46530c4742affe5b3e3a295c
goolord <zacharyachurchill@gmail.com>
2026-09-01 21:03:13 -0400

    refactor(sdl): remove vestigial C rasterizers and Shape module
    Delete unused software SDF rasterizers from nano_ui_ttf.c, strip manual batching geometry from nano_ui_batch.c, and drop the orphaned Shape.hs FFI wrappers. SDL rendering stays on CPU vertex emission + RenderGeometry.
    
    - add stripped cozette

--- sdl-lib/cbits/nano_ui_batch.h
+++ sdl-lib/cbits/nano_ui_batch.h
@@ -2,7 +2,6 @@
 #define NANO_UI_BATCH_H
 
 #include <SDL3/SDL.h>
-#include <stdbool.h>
 #include <stdint.h>
 
 typedef struct NanoUiBatch NanoUiBatch;
@@ -10,81 +9,6 @@ typedef struct NanoUiBatch NanoUiBatch;
 NanoUiBatch *nano_ui_batch_create(SDL_Renderer *renderer);
 void nano_ui_batch_destroy(NanoUiBatch *batch);
 void nano_ui_batch_flush(NanoUiBatch *batch);
-
-void nano_ui_batch_fill_solid(
-    NanoUiBatch *batch,
-    uint8_t r,
-    uint8_t g,
-    uint8_t b,
-    uint8_t a,
-    float x,
-    float y,
-    float w,
-    float h);
-
-bool nano_ui_batch_texture_dst(
-    NanoUiBatch *batch,
-    SDL_Texture *texture,
-    float tex_w,
-    float tex_h,
-    float dst_x,
-    float dst_y,
-    float dst_w,
-    float dst_h,
-    float u0,
-    float v0,
-    float u1,
-    float v1,
-    uint8_t r,
-    uint8_t g,
-    uint8_t b,
-    uint8_t a);
-
-bool nano_ui_batch_texture_sized(
-    NanoUiBatch *batch,
-    SDL_Texture *texture,
-    float x,
-    float y,
-    float w,
-    float h);
-
-void nano_ui_batch_triangle(
-    NanoUiBatch *batch,
-    uint8_t r,
-    uint8_t g,
-    uint8_t b,
-    uint8_t a,
-    float x0,
-    float y0,
-    float x1,
-    float y1,
-    float x2,
-    float y2);
-
-void nano_ui_batch_rounded_rect(
-    NanoUiBatch *batch,
-    uint8_t r,
-    uint8_t g,
-    uint8_t b,
-    uint8_t a,
-    float x,
-    float y,
-    float w,
-    float h,
-    float radius);
-
-void nano_ui_batch_stroke_rounded_rect(
-    NanoUiBatch *batch,
-    uint8_t r,
-    uint8_t g,
-    uint8_t b,
-    uint8_t a,
-    float x,
-    float y,
-    float w,
-    float h,
-    float radius,
-    float bw);
 
 void nano_ui_batch_draw_range(
     NanoUiBatch *batch,
